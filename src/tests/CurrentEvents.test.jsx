7796d9db7d912296df0be8847df947efa2bc99f1
Hamza Ali <73322608+hazaali2@users.noreply.github.com>
2021-06-17 16:12:20 -0400

    Update CurrentEvents.test.jsx

--- src/tests/CurrentEvents.test.jsx
+++ src/tests/CurrentEvents.test.jsx
@@ -9,7 +9,7 @@ import competitions from '../competitionData';
 it('checks for current competitions in the right group', () => {
   render(<CurrentEvents />);
   competitions.forEach((competition) => {
-    if (competition.endDate > new Date() || competition.endDate === null) {
+    if (new Date(competition.endDate) > new Date() || new Date(competition.endDate) === null) {
       expect(screen.getByText(competition.header)).toBeInTheDocument();
     }
   });
@@ -19,7 +19,7 @@ it('checks for current competitions in the right group', () => {
 it('checks for past competitions in the right group', () => {
   render(<PastEvents />);
   competitions.forEach((competition) => {
-    if (competition.endDate !== null && competition.endDate <= new Date()) {
+    if (new Date(competition.endDate) !== null && new Date(competition.endDate) <= new Date()) {
       expect(screen.getByText(competition.header)).toBeInTheDocument();
     }
   });
