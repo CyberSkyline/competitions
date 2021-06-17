11506a11c98a25ac5b8fd486245b79ff07a316e0
Hamza Ali <73322608+hazaali2@users.noreply.github.com>
2021-06-17 16:13:05 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable max-len */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import moment from 'moment';
-import { Segment, Grid, Divider, Header, Image, List } from 'semantic-ui-react';
+import { Card, Segment, Grid, Divider, Header, Image, List } from 'semantic-ui-react';
+import competitions from './competitionData';
 import './themes/style.css';
 import DismissibleMessage from './DismissibleMessage';
+import CustomCard from './CustomCard';
 import * as constants from './constants';
-import CurrentEvents from './CurrentEvents';
-import PastEvents from './PastEvents';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,34 +22,42 @@ ReactDOM.render(
     <div style={{ maxWidth: 1425, margin: 'auto', padding: '2em' }}>
       <DismissibleMessage />
 
-      <Divider horizontal section style={{ padding: '1em' }}>
+      <Divider horizontal section style={{ 'margin-top': '1em' }}>
         <Header size='large'>
           Current Events
         </Header>
       </Divider>
 
-      <CurrentEvents />
+      <Card.Group stackable itemsPerRow={4} doubling centered padded='vertically'>
+        {_.chain(competitions).sortBy('startDate').filter((element) => (element.endDate > new Date() || element.endDate === null)).map((element) => (
+          <CustomCard key={element.header} {...element} />
+        ))
+          .value()}
+      </Card.Group>
 
-      <Divider horizontal section style={{ padding: '1em' }}>
+      <Divider horizontal section style={{ 'margin-top': '4em' }}>
         <Header size='large'>
           Past Events
         </Header>
       </Divider>
 
-      <PastEvents />
+      <Card.Group stackable itemsPerRow={4} doubling centered padded='vertically'>
+        {_.chain(competitions).orderBy((competition) => moment(competition.startDate), 'desc').filter((element) => (element.endDate !== null && element.endDate <= new Date())).map((element) => (
+          <CustomCard key={element.header} {...element} />
+        ))
+          .value()}
+      </Card.Group>
     </div>
 
     <Segment className='Footer'>
       <Grid centered verticalAlign='middle'>
         <Grid.Column floated='left' mobile={5} tablet={3} computer={2} largeScreen={2} widescreen={2}>
-          <a href='https://cyberskyline.com' target='_blank' rel='noopener noreferrer'>
-            <Image src='/images/cyberskyline.svg' />
-          </a>
+          <Image src='/images/cyberskyline.svg' />
         </Grid.Column>
         <Grid.Column mobile={11} tablet={12} computer={12} largeScreen={12} widescreen={12}>
           <List horizontal centered divided floated='right'>
             <List.Item>
-              <a href='https://github.com/CyberSkyline/competitions/' target='_blank' rel='noopener noreferrer'>Want to add your CTF</a>
+              <a href='https://github.com/CyberSkyline/competitions/' target='_blank' rel='noreferrer'>Want to add your CTF</a>
             </List.Item>
             <List.Item>
               <span>{`Cyber Skyline ©  ${moment(new Date()).utc().format('YYYY')}`}</span>
